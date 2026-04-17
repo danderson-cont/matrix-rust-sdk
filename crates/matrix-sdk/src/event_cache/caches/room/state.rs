91d5b452c7bad4c000cf812ecdf30de1a1a75912
Ivan Enderlin <ivan@mnt.io>
2026-04-17 14:57:16 +0200

    doc(sdk): Remove an address `TODO`.

--- crates/matrix-sdk/src/event_cache/caches/room/state.rs
+++ crates/matrix-sdk/src/event_cache/caches/room/state.rs
@@ -1066,8 +1066,6 @@ impl<'a> RoomEventCacheStateLockWriteGuard<'a> {
         &mut self,
         root_event_id: OwnedEventId,
     ) -> Result<&mut ThreadEventCache, EventCacheError> {
-        // TODO: when there's persistent storage, try to lazily reload from disk, if
-        // missing from memory.
         let RoomEventCacheState {
             room_id,
             weak_room,
